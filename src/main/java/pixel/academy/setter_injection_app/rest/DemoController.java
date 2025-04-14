36e86f8608246855297696d1fa03a4a55269d672
Francesca Andrian <francescaandrianaaa@gmail.com>
2025-04-14 19:48:13 +0200

    Modify.

--- src/main/java/pixel/academy/setter_injection_app/rest/DemoController.java
+++ src/main/java/pixel/academy/setter_injection_app/rest/DemoController.java
@@ -1,6 +1,7 @@
 package pixel.academy.setter_injection_app.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pixel.academy.setter_injection_app.common.Chef;
